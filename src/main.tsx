728ff6fcb5feebac8a8b9299b6ffdc37dadf6830
YahyaOsama2612 <oyahya868@gmail.com>
2025-01-04 15:34:59 +0400

    Edit

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,10 @@
-import { createRoot } from 'react-dom/client'
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-    },
-  },
-});
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+import "./index.css";
+import App from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <QueryClientProvider client={queryClient}>
-  <App />
-</QueryClientProvider>
-)
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
